8b253ba8e9187fe48c384e0e41547dcd3332baf7
Mart Gjepali <gjepalimart@gmail.com>
2024-01-17 04:12:56 +0100

    banner fixed and signup

--- src/app/components/dialog-sign-in/IMPORT_DEPENDENCY.ts
+++ src/app/components/dialog-sign-in/IMPORT_DEPENDENCY.ts
@@ -1,4 +1,5 @@
 import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from '@angular/material/icon';
 
-export const IMPORT_DEPENDENCY = [MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, MatButtonModule]
+export const IMPORT_DEPENDENCY = [MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, MatButtonModule, MatIconModule]
