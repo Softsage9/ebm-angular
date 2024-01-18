8398e0b84fb18e84f9a5fba86f4e19dde587557e
Mart Gjepali <gjepalimart@gmail.com>
2024-01-18 03:14:08 +0100

    update

---
+++ src/app/components/contact/IMPORT_DEPENDENCY.ts
@@ -0,0 +1,2 @@
+import { MatButtonModule } from "@angular/material/button";
+export const IMPORT_DEPENDENCY = [MatButtonModule]
---
+++ src/app/components/frame16/IMPORT_DEPENDENCY.ts
@@ -0,0 +1,6 @@
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+export const IMPORT_DEPENDENCY = [MatExpansionModule, MatIconModule, MatButtonModule]
