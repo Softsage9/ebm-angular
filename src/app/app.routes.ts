172a183c45afabc121312ee1d181a5fab547fa04
Mart Gjepali <gjepalimart@gmail.com>
2024-01-19 15:00:37 +0100

    last-update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
+import { TestRouteComponent } from './test-route/test-route.component';
 
 export const routes: Routes = [
   {
     path: '', component: HomeComponent
   },
+  
+  {
+    path: 'test', component: TestRouteComponent
+  }
 ];
--- src/app/components/navbar/IMPORT_DEPENDENCY.ts
+++ src/app/components/navbar/IMPORT_DEPENDENCY.ts
@@ -1,5 +1,6 @@
 import { MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 
-export const IMPORT_DEPENDENCY = [MatDialogModule, CommonModule]
+export const IMPORT_DEPENDENCY = [MatDialogModule, CommonModule, RouterModule]
